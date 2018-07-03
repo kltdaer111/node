415fbce748322b377cfa880f17a8e325d4d2da5c
root <root@localhost.localdomain>
2018-07-03 21:01:26 +0800

    first

--- statistics_gen.js
+++ statistics_gen.js
@@ -16,29 +16,32 @@ var log_dbconfig = {
 	database : 'wangmeijie_trunk_sg_log',
 };
 
-var res = {
-	'glog' : false,
-	'log' : false,
-};
+var res = {};
 
 res.reset = function(){
-	this.glog = false;
-	this.log = false;
+	this.task_data = false;
+	this.account_data = false;
+	this.login_data = false;
 }
 
-res.set_glog_data = function(data){
-	this.glog = data;
+res.reset();
+
+res.set_account_data = function(data){
+	this.account_data = data;
 }
 
-res.set_log_data = function(data){
-	this.log = data;
+res.set_task_data = function(data){
+	this.task_data = data;
+}
+
+res.set_login_data = function(data){
+	this.login_data = data;
 }
 
 res.process_data_if_get_all = function(){
-	if(this.glog !== false && this.log !== false){
+	if(this.account_data !== false && this.task_data !== false && this.login_data !== false){
 		console.log(11111111);
-		var start_of_today = moment().startOf('day');
-		console.log(start_of_today.unix(), start_of_today.subtract(1, 'day').unix());
+		
 
 
 		this.reset();
@@ -47,18 +50,26 @@ res.process_data_if_get_all = function(){
 
 function logic(){
 	var con_glog = mysql.createConnection(glog_dbconfig);
-	var now = new Date();
-	con_glog.query('SELECT * FROM account_log', function(error, results, fields){
+	var start_of_today = moment().startOf('day');
+	var timestamp_today_begin = start_of_today.unix();
+	var day_seconds = 24 * 60 * 60;
+	var timestamp_yesterday_begin = timestamp_today_begin - day_seconds;
+	var timestamp_tdby_begin = timestamp_yesterday_begin - day_seconds;
+	con_glog.query('SELECT * FROM account_log WHERE UNIX_TIMESTAMP(create_date)=? AND (source_type=1562 OR source_type=1560);', [timestamp_tdby_begin],function(error, results, fields){
 		//console.log(results[0]);
-		res.set_glog_data(results);
+		res.set_account_data(results);
 		res.process_data_if_get_all();
 	});
 
 	var con_log = mysql.createConnection(log_dbconfig);
-	con_log.query('SELECT * FROM task_log', function(error, results, fields){
+	con_log.query('SELECT * FROM task_log WHERE task_type=1 AND op_type=702 AND UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?;', [timestamp_tdby_begin, timestamp_yesterday_begin], function(error, results, fields){
 		//console.log(results[1]);
-		res.set_log_data(results);
+		res.set_task_data(results);
 		res.process_data_if_get_all();
+	});
+
+	con_log.query('SELECT * FROM login_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?;', [timestamp_yesterday_begin, timestamp_today_begin], function(error, results, fields){
+
 	});
 }
 
