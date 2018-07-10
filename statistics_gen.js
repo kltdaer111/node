914db5042c88e308b84b1fc3d441d6a76cadf00e
root <root@localhost.localdomain>
2018-07-10 20:10:56 +0800

    statistics using node

--- statistics_gen.js
+++ statistics_gen.js
@@ -1,45 +1,122 @@
 const mysql = require('mysql');
-const schedule = require('node-schedule');
-const moment = require('moment');
-const layer_data_processor = require('./layer_data_processor');
-const LayerDataProcessor = layer_data_processor.LayerDataProcessor;
 
-const util = require('util');
+function NewUserGen(con_glog, day_start_time){
+    this.con_glog = con_glog;
 
-var glog_dbconfig = {
-	host : '139.196.41.108',
-	user : 'wsy',
-	password : 'Wsy1985!',
-	database : 'wai_trunk_sg_glog',
+    this.new_mobile_count = 0;
+    this.new_account_count = 0;
+    this.reg_mobile_count = 0;
+    this.new_role_count = 0;
+    
+    this.cov_rate = 0;
+    this.start_time = day_start_time;
+    this.end_time = day_start_time + 24 * 60 * 60;
 };
 
-var log_dbconfig = {
-	host : '192.168.1.5',
-	user : 'root',
-	password : 'Sanguo1!',
-	database : 'nei_trunk_sg_log',
-};
+NewUserGen.prototype.gen = function(){
+    var self = this;
+    //新增设备
+    self.con_glog.query("SELECT COUNT(id) AS quantity FROM mobile_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?;", [self.start_time, self.end_time], function(err, results, fields){
+        console.log(results);
+        self.new_mobile_count = results[0].quantity;
+        
 
-var start_of_today = moment().startOf('day');
-var timestamp_today_begin = start_of_today.unix();
-var day_seconds = 24 * 60 * 60;
-var timestamp_yesterday_begin = timestamp_today_begin - day_seconds;
-var timestamp_tdby_begin = timestamp_yesterday_begin - day_seconds;
+        //新增账号 注册设备
+        self.con_glog.query("SELECT COUNT(DISTINCT account_uid) AS quantity1, COUNT(DISTINCT mobile_uuid) AS quantity2 FROM account_log WHERE source_type=1560 AND UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?;", [self.start_time, self.end_time], function(err, results, fields){
+            console.log(results);
+            self.new_account_count = results[0].quantity1;
+            self.reg_mobile_count = results[0].quantity2;
 
-function compare_equal(val1, val2){
-	return val1 == val2;
+            //新增角色
+            self.con_glog.query("SELECT COUNT(DISTINCT role_uid) AS quantity FROM account_log WHERE source_type=1562 AND UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?;", [self.start_time, self.end_time], function(err, results, fields){
+                console.log(results);
+                self.new_role_count = results[0].quantity;
+
+                //插入数据库
+                self.con_glog.query("REPLACE INTO `new_user_log`(`time`, `mobile`, `reg_mobile`, `account`, `role`, `rate`) VALUES(find_date, new_mobile_count, reg_mobile_count, new_account_count, new_role_count, cov_rate);", [], function(err, results, fields){
+                    if(err){
+                        console.log(err);
+                    }
+                })
+            });
+        });
+    });
+
 }
 
-function compare_lessequal(val1, val2){
-	return val1 <= val2;
+function ActiveUserGen(con_glog, day_start_time){
+    this.con_glog = con_glog;
+    this.start_time = day_start_time;
+    this.end_time = day_start_time + 24 * 60 * 60;
 }
 
-function compare_greater(val1, val2){
-	return val1 > val2;
+ActiveUserGen.prototype.get_need_data = function(begin, end, callback){
+    var self = this;
+    this.con_glog.query("SELECT COUNT(DISTINCT account_uid) FROM account_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?", [begin, end], function(err, results, fields){
+        //console.log(results);
+
+        callback.call(self, results);
+    });
 }
 
-function Percentage(number1, number2) { 
-    return (Math.round(number1 / number2 * 10000) / 100.00 + "%");// 小数点后两位百分比
+ActiveUserGen.prototype.gen = function(){
+    //日活跃
+    this.get_need_data(this.start_time, this.end_time, function(data){
+        console.log(data);
+
+    });
+    //周活跃
+    this.get_need_data(this.end_time - 7 * 24 * 60 * 60, this.end_time, function(data){
+        console.log(data);
+    });
+    //月活跃
+    this.get_need_data(this.end_time - 30 * 24 * 60 * 60, this.end_time, function(data){
+        console.log(data);
+    });
+}
+
+function RemainUser(con_glog, day_start_time){
+    this.con_glog = con_glog;
+    this.start_time = day_start_time;
+    this.end_time = day_start_time + 24 * 60 * 60;
+}
+
+RemainUser.prototype.get_need_data = function(interval_day, callback){
+    var new_date_start = this.end_time - interval_day * 24 * 60 * 60;
+    var new_date_end = new_date_start + 24 * 60 * 60;
+    var self = this;
+    self.con_glog.query("SELECT COUNT(DISTINCT account_uid) AS quantity FROM account_log WHERE source_type=1560 AND UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?", [new_date_start, new_date_end], function(err, results, fields){
+        console.log(results);
+
+        self.con_glog.query("SELECT COUNT(t1.account_uid) AS quantity FROM (SELECT DISTINCT account_uid FROM account_log WHERE source_type=1560 AND UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?) AS t1 INNER JOIN (SELECT DISTINCT account_uid FROM account_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?) AS t2 ON t1.account_uid=t2.account_uid;", [new_date_start, new_date_end, self.start_time, self.end_time], function(err, results, fields){
+            callback.call(self, results);
+        });
+    });
+
+    
+}
+
+RemainUser.prototype.gen = function(){
+    //生成昨天的次日留存
+    this.get_need_data(2, function(data){
+        console.log(data);
+    });
+    //生成七天前的七日留存
+    this.get_need_data(7, function(data){
+        console.log(data);
+    });
+    //生成30天前的月留存
+    this.get_need_data(30, function(data){
+        console.log(data);
+    });
+}
+
+function ComeBackUser(con_glog, active_time_begin, back_time_begin){
+    this.con_glog = con_glog;
+    this.active_time_begin = active_time_begin;
+    this.active_time_end = active_time_begin + 24 * 60 * 60;
+    this.back_time_begin = back_time_begin;
+    this.back_time_end = back_time_begin + 24 * 60 * 60;
 }
 
 function fillSqlIn(sql, symbol, array){
@@ -51,338 +128,33 @@ function fillSqlIn(sql, symbol, array){
 	return sql.replace(symbol, tmp);
 }
 
-function Count(obj){
-	var i = 0;
-	for(idx in obj){
-		i += 1;
-	}
-	return i;
+ComeBackUser.prototype.gen = function (){
+    //活跃账号数量
+    var self = this;
+    this.con_glog.query("SELECT COUNT(DISTINCT account_uid) AS quantity FROM account_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?", [self.active_time_begin, self.active_time_end], function(err, results, fields){
+        console.log(results);
+        
+        //活跃账号中的流失账号
+        self.con_glog.query("SELECT t1.account_uid FROM (SELECT DISTINCT account_uid FROM account_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?) AS t1 LEFT JOIN (SELECT DISTINCT account_uid FROM account_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?) AS t2 ON t1.account_uid=t2.account_uid WHERE t2.account_uid IS NULL", [self.active_time_begin, self.active_time_end, self.active_time_end, self.back_time_begin], function(err, need_result, fields){
+            if(err){
+                console.log(err);
+            }
+            console.log(need_result);
+            
+            var sql = "SELECT COUNT(DISTINCT account_uid) AS quantity FROM account_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<? AND account_uid IN (&)";
+            sql = fillSqlIn(sql, '&', need_result);
+            self.con_glog.query(sql, [self.back_time_begin, self.back_time_end], function(err, results, fields){
+                if(err){
+                    console.log(err);
+                }
+                console.log(results);
+            })
+        });
+
+    });
 }
 
-
-var task_table = ['栏名', '达成人数', '相对达成率', '绝对达成率', '离开人数', '次日留存', '留存率'];
-
-function Worker(processor){
-	this.processor = processor;
-	this.show_data = [];
-	this.result_data = [];
-	this.sum_for_check = 0;
-}
-
-Worker.prototype.start_work = function(type, col_table){
-	this.type = type;
-	if(col_table === undefined){
-		this.col_table = task_table;
-	}
-	else{
-		this.col_table = col_table;
-	}
-	this.con_glog = mysql.createConnection(glog_dbconfig);
-	this.con_log = mysql.createConnection(log_dbconfig);
-	this.gen_filter_data(this.get_mysql_data_for_processor);
-}
-
-Worker.prototype.getSecondDayRemainingByAccountid = function(account_array, second_day_start_timestamp, para1, callback){
-	if(Count(account_array) === 0){
-		callback.call(this, null, para1, []);
-		return;
-	}
-	var sql = "SELECT account_uid FROM account_log WHERE UNIX_TIMESTAMP(create_date)=? AND account_uid IN (&) GROUP BY account_uid;";
-	sql = fillSqlIn(sql, '&', account_array);
-	// console.log(sql);
-	const self = this;
-	this.con_glog.query(sql, [second_day_start_timestamp], function(error, results, fields){
-		//console.log(results);
-		callback.call(self, error, para1, results);
-	});
-}
-
-Worker.prototype.getSecondDayRemainingByRoleid = function(role_array, second_day_start_timestamp, para1, callback){
-	if(Count(role_array) === 0){
-		callback.call(this, null, para1, []);
-		return;
-	}
-	var sql = "SELECT role_uid FROM login_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<? AND role_uid IN (&) GROUP BY role_uid;";
-	sql = fillSqlIn(sql, '&', role_array);
-	// console.log(sql);
-	// console.log(role_array);
-	const self = this;
-	this.con_log.query(sql, [second_day_start_timestamp, second_day_start_timestamp + 24 * 60 * 60], function(error, results, fields){
-		//console.log(results);
-		callback.call(self, error, para1, results);
-	});
-}
-
-Worker.prototype.do_with_SecondDayRemaining_result = function(err, idx, result){
-	if(err){
-		console.log(err);
-		return;
-	}
-	this.result_data[idx].push(result);
-	//console.log(result);
-	//console.log(sum_for_check);
-	this.sum_for_check -= 1;
-	var res = [];
-	if(this.sum_for_check === 0){
-		this.do_with_results();
-	}
-}
-
-Worker.prototype.get_col_data = function get_col_data(type){
-	var data = [];
-	switch(type){
-		case '栏名':
-		{
-			for(idx in this.result_data){
-				data.push(this.result_data[idx][2]);
-			}
-		}
-		break;
-		case '达成人数':
-		{
-			for(idx in this.result_data){
-				data.push(Count(this.result_data[idx][1]));
-			}
-		}
-		break;
-		case '相对达成率':
-		{
-			var denominator = Count(this.result_data[0][1]);
-			for(idx in this.result_data){
-				if(denominator === 0){
-					data.push(0);
-				}
-				else{
-					var val = Count(this.result_data[idx][1]);
-					data.push(Percentage(val, denominator));
-					denominator = val;
-				}
-			}
-		}
-		break;
-		case '绝对达成率':
-		{
-			var denominator = Count(this.result_data[0][1]);
-			for(idx in this.result_data){
-				if(denominator === 0){
-					data.push(0);
-				}
-				else{
-					var val = Count(this.result_data[idx][1]);
-					data.push(Percentage(val, denominator));
-				}
-			}
-		}
-		break;
-		case '离开人数':
-		{
-			var last_num = Count(this.result_data[0][1]);
-			for(idx in this.result_data){
-				var val = Count(this.result_data[idx][1]);
-				data.push(last_num - val);
-				last_num = val;
-			}
-		}
-		break;
-		case '次日留存':
-		{
-			for(idx in this.result_data){
-				var val = Count(this.result_data[idx][3]);
-				data.push(val);
-			}
-		}
-		break;
-		case '留存率':
-		{
-			for(idx in this.result_data){
-				var buf = this.result_data[idx];
-				var val1 = Count(buf[1]);
-				var val2 = Count(buf[3]);
-				if(val1 == 0){
-					data.push(Percentage(0, 1));
-				}
-				else{
-					data.push(Percentage(val2, val1));
-				}
-			}
-		}
-		break;
-	}
-	return data;
-}
-
-Worker.prototype.do_with_results = function(){
-	for(idx in this.col_table){
-		this.show_data.push(this.get_col_data(this.col_table[idx]));
-	}
-	var str = JSON.stringify(this.show_data);
-	//var buf = Buffer.from(str);
-	this.con_log.query("INSERT INTO daily_statistics_log(type,data,date) VALUES(?,?,FROM_UNIXTIME(?));", [this.type, str, timestamp_tdby_begin], function(err, results, fields){
-		if(err){
-			throw err;
-		}
-		console.log('OK');
-	});
-	console.log(this.result_data);
-	console.log(this.show_data);
-	//console.log(this.type);
-}
-
-Worker.prototype.fill_result_if_ready = function(){
-	if(this.processor.checkMysqlDataReady()){
-		this.result_data = this.processor.getChainWorkResult();
-		//console.log(results);
-		for(idx in this.result_data){
-			this.sum_for_check += 1;
-			switch(this.result_data[idx][0]){
-				case 'account_uid':
-				{
-					this.getSecondDayRemainingByAccountid(this.result_data[idx][1], timestamp_yesterday_begin, idx, this.do_with_SecondDayRemaining_result);
-				}
-				break;
-				case 'role_uid':
-				{
-					this.getSecondDayRemainingByRoleid(this.result_data[idx][1], timestamp_yesterday_begin, idx, this.do_with_SecondDayRemaining_result);
-				}
-				break;
-			}
-		}
-	}
-}
-
-Worker.prototype.get_mysql_data_for_processor = function(){
-	//取出指定日期的数据
-	this.processor.regNeedMysqlDataName('account');
-	this.processor.regNeedMysqlDataName('login');
-	this.processor.regNeedMysqlDataName(this.type);
-	const self = this;
-	this.con_glog.query('SELECT * FROM account_log WHERE UNIX_TIMESTAMP(create_date)=? AND (source_type=1562 OR source_type=1560);', [timestamp_tdby_begin], function(error, results, fields){
-		//console.log(results[0]);
-		if(error){
-			throw error;
-		}
-		self.processor.insertMysqlRawResults('account', results);
-		self.fill_result_if_ready();
-	});
-	this.con_log.query('SELECT * FROM login_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?;', [timestamp_yesterday_begin, timestamp_today_begin], function(error, results, fields){
-		if(error){
-			throw error;
-		}
-		self.processor.insertMysqlRawResults('login', results);
-		self.fill_result_if_ready();
-	});
-	switch(this.type){
-		case 'task':{
-			this.con_log.query('SELECT * FROM task_log WHERE task_type=1 AND op_type=702 AND UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?;', [timestamp_tdby_begin, timestamp_yesterday_begin], function(error, results, fields){
-				//console.log(results[1]);
-				if(error){
-					throw error;
-				}
-				self.processor.insertMysqlRawResults('task', results);
-				self.fill_result_if_ready();
-			});
-		}
-		break;
-		case 'level':{
-			this.con_log.query('SELECT * FROM role_exp_level_log WHERE old_level!=new_level AND source_param != 0 AND UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<?;', [timestamp_tdby_begin, timestamp_yesterday_begin], function(error, results, fields){
-				//console.log(results[1]);
-				if(error){
-					throw error;
-				}
-				self.processor.insertMysqlRawResults('level', results);
-				self.fill_result_if_ready();
-			});
-		}
-		break;
-		case 'duration':{
-			this.con_log.query('SELECT role_uid,sum(online_time) as duration FROM login_log WHERE UNIX_TIMESTAMP(create_time)>=? AND UNIX_TIMESTAMP(create_time)<? GROUP BY role_uid;', [timestamp_tdby_begin, timestamp_yesterday_begin], function(error, results, fields){
-				//console.log(results[1]);
-				if(error){
-					throw error;
-				}
-				self.processor.insertMysqlRawResults('duration', results);
-				self.fill_result_if_ready();
-			});
-		}
-		break;
-	}
-	
-}
-
-Worker.prototype.gen_filter_data = function(callback){
-	//注册
-	this.processor.pushFilterToChain('account', 'source_type', 1560, compare_equal, 'mobile_uuid', 'account_uid', '注册');
-	//创角
-	this.processor.pushFilterToChain('account', 'source_type', 1562, compare_equal, 'account_uid', 'role_uid', '创建角色');
-	
-	const self = this;
-	switch(this.type){
-		//任务
-		//获取主线任务id序列 --保险起见应改为配置表读取
-		case 'task':{
-			this.con_log.query('SELECT task_id FROM `task_log` AS t5 RIGHT JOIN (SELECT COUNT(*) AS cola,t4.role_uid FROM (SELECT id,t3.role_uid,task_id FROM `task_log` AS t3 LEFT OUTER JOIN (SELECT role_uid FROM (SELECT role_uid,MIN(UNIX_TIMESTAMP(create_time)) as firsttime FROM `task_log` GROUP BY role_uid) as t1 WHERE t1.firsttime>=? AND t1.firsttime<?) AS t2 ON t2.role_uid=t3.role_uid WHERE task_type=1 AND op_type=702) AS t4 GROUP BY t4.role_uid ORDER BY cola DESC LIMIT 1) AS t6 ON t5.role_uid=t6.role_uid WHERE task_type=1 AND op_type=702 ORDER BY id;', [timestamp_tdby_begin, timestamp_yesterday_begin], function(error, results, fields){
-				//console.log(results);
-				for(idx in results){
-					var task_id = results[idx]['task_id'];
-					if(task_id == 0){
-						continue;
-					}
-					var name = '任务' + task_id;
-					self.processor.pushFilterToChain('task', 'task_id', task_id, compare_equal, 'role_uid', 'role_uid', name);
-				}
-				if(callback !== undefined){
-					callback.call(self);
-				}
-			});
-		}
-		break;
-		case 'duration':{
-			var duration_cfg = [1, 3, 10, 30, 60];
-			var name = '';
-			for(idx in duration_cfg){
-				var value = duration_cfg[idx];
-				var sec = value * 60;
-				name = '>' + value + 'min';
-				self.processor.pushFilterToChain('duration', 'duration', sec, compare_greater, 'role_uid', 'role_uid', name);
-			}
-			if(callback !== undefined){
-				callback.call(self);
-			}		
-		}
-		break;
-		case 'level':{
-			for(var i = 2; i <= 100; i++){
-				var name = 'lv' + i;
-				self.processor.pushFilterToChain('level', 'new_level', i, compare_equal, 'role_uid', 'role_uid', name);
-			}
-			if(callback !== undefined){
-				callback.call(self);
-			}		
-		}
-		break;
-	}
-}
-
-schedule.scheduleJob('0 0 4 * * *', function(){
-	var processor1 = new LayerDataProcessor();
-	var worker1 = new Worker(processor1);
-	var processor2 = new LayerDataProcessor();
-	var worker2 = new Worker(processor2);
-	var processor3 = new LayerDataProcessor();
-	var worker3 = new Worker(processor3);
-
-	worker1.start_work('level');
-	worker2.start_work('duration');
-	worker3.start_work('task');
-});
-
-var processor1 = new LayerDataProcessor();
-var worker1 = new Worker(processor1);
-var processor2 = new LayerDataProcessor();
-var worker2 = new Worker(processor2);
-var processor3 = new LayerDataProcessor();
-var worker3 = new Worker(processor3);
-
-worker1.start_work('level');
-worker2.start_work('duration');
-worker3.start_work('task');
+module.exports.NewUserGen = NewUserGen;
+module.exports.ActiveUserGen = ActiveUserGen;
+module.exports.RemainUser = RemainUser;
+module.exports.ComeBackUser = ComeBackUser;
